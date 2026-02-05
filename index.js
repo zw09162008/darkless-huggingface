f12441f4f06f07672f2dd04f0157b7423adfe8ee
zw09162008 <zw09162008@gmail.com>
2026-02-05 11:17:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,18 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.status(200).send('<h1>Server is running!</h1>');
-});
-
+app.get('/', (req, res) => res.status(200).send('<h1>Server Online</h1>'));
 app.get('/sub', (req, res) => {
   const subFile = path.join(__dirname, 'sub.txt');
-  if (fs.existsSync(subFile)) {
-    res.sendFile(subFile);
-  } else {
-    res.status(404).send('Wait 1 min for nodes...');
-  }
+  if (fs.existsSync(subFile)) res.sendFile(subFile);
+  else res.status(404).send('Still generating nodes, wait 10s...');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => console.log(`HTTP Server running on port ${port}`));
 
-// 使用 spawn 实时输出后台脚本日志
+// 关键：实时透传后台脚本日志
 const child = spawn('sh', ['./entrypoint.sh']);
 
-child.stdout.on('data', (data) => {
-  console.log(`STDOUT: ${data}`);
-});
-
-child.stderr.on('data', (data) => {
-  console.error(`STDERR: ${data}`);
-});
-
-child.on('close', (code) => {
-  console.log(`Child process exited with code ${code}`);
-});
+child.stdout.on('data', (data) => console.log(`[SCRIPT STDOUT]: ${data}`));
+child.stderr.on('data', (data) => console.error(`[SCRIPT ERR]: ${data}`));
+child.on('close', (code) => console.log(`Script exited with code ${code}`));
