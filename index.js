b5383e16995cde5a1e1f4ecd4e631ad8cf24b49f
zw09162008 <zw09162008@gmail.com>
2026-02-05 10:57:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      // 是否开启自动
 const WSPATH = process.env.WSPATH || UUID.slice(0, 8);     // 节点路径，默认获取uuid前8位
 const SUB_PATH = process.env.SUB_PATH || 'sub';            // 获取节点的订阅路径
 const NAME = process.env.NAME || '';                       // 节点名称
-const PORT = process.env.PORT || 3000;                     // http和ws服务端口
+const PORT = process.env.PORT || 8080;                     // http和ws服务端口
 
 let uuid = UUID.replace(/-/g, ""), CurrentDomain = DOMAIN, Tls = 'tls', CurrentPort = 443, ISP = '';
 const DNS_SERVERS = ['8.8.4.4', '1.1.1.1'];
